817331bd7ed4b8590f00b0f03a4ee65e06d68981
Ben Rehouma Rayen <rayenebenrehouma@outlook.fr>
2023-05-13 12:37:01 +0200

    Finalisation update figure

---
+++ src/Form/UpdateFigureFormType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Figure;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class UpdateFigureFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('groupe', ChoiceType::class, [
+                'choices' => [
+                    'Grabs' => 'Grabs',
+                    'Rotations' => 'Rotations',
+                    'Flips' => 'Flips',
+                    'Rotations désaxées' => 'Rotations désaxées',
+                    'Slides' => 'Slides'
+                ]
+            ])
+            ->add('illustration', FileType::class,[
+                'label' => 'Tricks Image',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/*',
+                        ],
+                        'mimeTypesMessage' => 'Le fichier choisi n\'est pas au bon format (Format accepté jpg, png ,jpeg)',
+                    ])
+                ],
+            ])
+            ->add('video')
+            ->add('submit', SubmitType::class, [
+                'label' => "Mettre à jour",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Figure::class,
+        ]);
+    }
+}
