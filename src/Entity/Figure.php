270b1f63da003be7a50a9459c825e1b6f96490f3
Ben Rehouma Rayen <rayenebenrehouma@outlook.fr>
2023-07-11 16:00:10 +0200

    finalisation de l'application

--- src/Entity/Figure.php
+++ src/Entity/Figure.php
@@ -29,7 +29,7 @@ class Figure
     #[ORM\Column(length: 255)]
     private ?string $video = null;
 
-    #[ORM\OneToMany(mappedBy: 'id_figure', targetEntity: Commentaire::class)]
+    #[ORM\OneToMany(mappedBy: 'id_figure', targetEntity: Commentaire::class, cascade: ["remove"])]
     private Collection $commentaires;
 
     #[ORM\Column]
@@ -228,4 +228,5 @@ class Figure
 
         return $this;
     }
+
 }
